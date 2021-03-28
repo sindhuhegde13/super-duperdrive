8b0cb98493177a297640497477d4d09ac9e10dd5
sindhuhegde13 <hegdesindhu13@gmail.com>
2021-03-28 13:48:37 +0530

    First commit

---
+++ src/test/java/com/nanodegree/superduperdrive/SuperduperdriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nanodegree.superduperdrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperduperdriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
